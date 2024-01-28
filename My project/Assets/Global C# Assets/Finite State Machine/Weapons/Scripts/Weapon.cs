bfe5f28a24e69db23dbd2eac0daa1a40e2c977f9
FoxParadeAFK <151231520+FoxParadeAFK@users.noreply.github.com>
2024-01-28 22:59:40 +0000

    2 - Connection From Weapons To Animator

--- My project/Assets/Global C# Assets/Finite State Machine/Weapons/Scripts/Weapon.cs
+++ My project/Assets/Global C# Assets/Finite State Machine/Weapons/Scripts/Weapon.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-    public class Weapon : MonoBehaviour
-    {
-        public void Enter() {
-            print($"{transform.name} enter");
-        }
+public class Weapon : MonoBehaviour
+{
+    private Animator anim;
+    private GameObject baseGameObject;
+    public void Enter() {
+        print($"{transform.name} enter");
+
+        anim.SetBool("active", true);
     }
+
+    private void Awake() {
+        baseGameObject =  transform.Find("Base").gameObject;
+        anim = baseGameObject.GetComponent<Animator>();
+    }
+}
