3eeb44957a2261949183cd012c7a2701c30087fb
FoxParadeAFK <151231520+FoxParadeAFK@users.noreply.github.com>
2024-01-28 22:35:21 +0000

    2 - Animations & Clips Imports

--- My project/Assets/Global C# Assets/Finite State Machine/Weapons/Scripts/Weapon.cs
+++ My project/Assets/Global C# Assets/Finite State Machine/Weapons/Scripts/Weapon.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
-{
-    public void Enter() {
-        print($"{transform.name} enter");
+    public class Weapon : MonoBehaviour
+    {
+        public void Enter() {
+            print($"{transform.name} enter");
+        }
     }
-}
