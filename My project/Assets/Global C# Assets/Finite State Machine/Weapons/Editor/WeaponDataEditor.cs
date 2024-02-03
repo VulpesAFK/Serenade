3e0df177eb6b56c46f694ef601ebb0e6025c8970
FoxParadeAFK <151231520+FoxParadeAFK@users.noreply.github.com>
2024-02-03 17:46:11 +0000

    9 - Completed Assembly Filter

--- My project/Assets/Global C# Assets/Finite State Machine/Weapons/Editor/WeaponDataEditor.cs
+++ My project/Assets/Global C# Assets/Finite State Machine/Weapons/Editor/WeaponDataEditor.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEngine;
@@ -13,7 +14,7 @@ namespace FoxTail
 
         // Used as a means of defining a universal type of data in the C# built-in system
         // Can store the information of other classes and not just the object that contain them 
-        private List<Type> dataComponentType = new List<Type>();
+        private static List<Type> dataComponentType = new List<Type>();
 
         // This Unity function that holds the basic standard value of the editor 
         // Manipulate to show the objects we want
@@ -32,6 +33,20 @@ namespace FoxTail
         [DidReloadScripts]
         private static void OnRecompile() {
             // Search all the app domain and assemblies and retrieve 
+            // Stores a list of all assemblies that are loaded in the current app doamin
+            // Assembly => A unit of code that is compiled and ran in the .NET runtime thus the types of class and functions and interfaces and ect..
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            // Loops through the list and create a list that stores all assemblies that met these conditions
+            // Looks at all the assemblies in assemblies and stores
+            var types = assemblies.SelectMany(assemblies => assemblies.GetTypes());
+
+            // Look for all types that inherit component data
+            // Adds to var of types where the the type is a subclass/child of the component data
+            // Additional filter to prevent the same component data generic class from being allowed in
+            var filteredTypes = types.Where(type => type.IsSubclassOf(typeof(ComponentData)) && !type.ContainsGenericParameters && type.IsClass);
+
+            // Add finally to the final static main list
+            dataComponentType = filteredTypes.ToList();
         }
     }
 }
