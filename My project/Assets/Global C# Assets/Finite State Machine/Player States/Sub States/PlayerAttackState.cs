56ec37197fdbb5cb5416782a604c4688a76a3553
FoxParadeAFK <151231520+FoxParadeAFK@users.noreply.github.com>
2024-02-04 11:11:38 +0000

    Commenting

--- My project/Assets/Global C# Assets/Finite State Machine/Player States/Sub States/PlayerAttackState.cs
+++ My project/Assets/Global C# Assets/Finite State Machine/Player States/Sub States/PlayerAttackState.cs
@@ -4,21 +4,18 @@ using UnityEngine;
 
 public class PlayerAttackState : PlayerAbilitiesState
 {
+    // Main weapon script link
     private Weapon weapon;
-    public PlayerAttackState(Player player,
-        PlayerStateMachine stateMachine,
-        PlayerData playerData,
-        string animBoolName,
-        Weapon weapon
-    ) : base(player, stateMachine, playerData, animBoolName) {
+    public PlayerAttackState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName, Weapon weapon) : base(player, stateMachine, playerData, animBoolName) {
+        // Base set weapon to the initialized
         this.weapon = weapon;
 
         weapon.OnExit += ExitHandler;
     }
 
-    public override void Enter()
-    {
+    public override void Enter() {
         base.Enter();
+        // Start weaponry logic equal to when the state enters
         weapon.Enter();
     }
 
