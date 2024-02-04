c5a6bc36545f4d0edaac4527e33ece884d9a1911
FoxParadeAFK <151231520+FoxParadeAFK@users.noreply.github.com>
2024-02-04 12:46:13 +0000

    10 - LayerMasking & Core Fetching Generics

--- My project/Assets/Global C# Assets/Finite State Machine/Weapons/Scriptable Objects/Component Data/ActionHitBoxData.cs
+++ My project/Assets/Global C# Assets/Finite State Machine/Weapons/Scriptable Objects/Component Data/ActionHitBoxData.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 namespace FoxTail
 {
-    public class ActionHitBoxData : ComponentData<AttackActionHitBox>
-    {
-
+    public class ActionHitBoxData : ComponentData<AttackActionHitBox> {
+        // Data that will be defined for all of the attack hitbox data assigned
+        [field: SerializeField] public LayerMask DetectableLayers { get; private set; }
     }
 }
--- My project/Assets/Global C# Assets/Finite State Machine/Weapons/Scriptable Objects/Component Data/Attack Data/AttackActionHitBox.cs
+++ My project/Assets/Global C# Assets/Finite State Machine/Weapons/Scriptable Objects/Component Data/Attack Data/AttackActionHitBox.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 namespace FoxTail
 {
     [Serializable]
-    public class AttackActionHitBox : AttackData
-    {
-
+    public class AttackActionHitBox : AttackData {
+        // These are properties that will have to be defined for each weaponry attack
+        [field: SerializeField] public Rect HitBox { get; private set; }
     }
 }
