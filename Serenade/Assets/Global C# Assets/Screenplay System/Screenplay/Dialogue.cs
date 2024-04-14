bf33aa408a049abd25a072c056e47aed659124ff
FoxParadeAFK <151231520+FoxParadeAFK@users.noreply.github.com>
2024-04-14 20:28:21 +0100

    New Data + Dialogue

--- Serenade/Assets/Global C# Assets/Screenplay System/Screenplay/Dialogue.cs
+++ Serenade/Assets/Global C# Assets/Screenplay System/Screenplay/Dialogue.cs
@@ -7,26 +7,42 @@ using UnityEngine;
 namespace FoxTail.Serenade.Experimental.Screenplay.System {
     public class Dialogue : ScriptDialogue {
         private String empty;
+        private bool textFinished = true;
         private IEnumerator textEffect;
-        
+        [SerializeField] private ScriptDialogueData dialogueData;
+
+        private int textCounter_;
+        private bool readFinished;
         protected override void Update()
         {
             base.Update();
 
-            if (interactiveInput && textEffect == null) {
-                textEffect = textTypeEffect("Hello there, this is a test message just to this silly system");
-                StartCoroutine(textEffect);
+            if (interactiveInput && textFinished) {
+                // textCounter_ = textCounter_ > dialogueData.Scripts.Length? 0 : textCounter_;
+
+                if (textCounter_ >= dialogueData.Scripts.Length) {
+                    canvasText.text = "";
+                    textCounter_ = 0;
+                }
+                else {
+                    textEffect = textTypeEffect(dialogueData.Scripts[textCounter_].Text);
+                    StartCoroutine(textEffect);
+                    textCounter_++;
+                }
             }
         }
 
         IEnumerator textTypeEffect(String textType) {
             empty = "";
+            textFinished = false;
 
             for (int i = 0; i < textType.Length; i++) {
                 empty = empty + textType[i];
                 canvasText.text = empty;
                 yield return new WaitForSeconds(0.05f);
             }
+
+            textFinished = true;
         }
     }
 }
