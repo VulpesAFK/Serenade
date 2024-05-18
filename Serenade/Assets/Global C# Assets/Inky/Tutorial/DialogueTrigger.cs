47c97557c9a95821e18c6885c5a372c3d5a24189
FoxParadeAFK <151231520+FoxParadeAFK@users.noreply.github.com>
2024-05-19 00:29:19 +0100

    Trigger

--- Serenade/Assets/Global C# Assets/Inky/Tutorial/DialogueTrigger.cs
+++ Serenade/Assets/Global C# Assets/Inky/Tutorial/DialogueTrigger.cs
@@ -2,20 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace FoxTail
-{
-    public class DialogueTrigger : MonoBehaviour
-    {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
+namespace FoxTail {
+    public class DialogueTrigger : MonoBehaviour {
+        [SerializeField] private GameObject visualCue;
+        private Collider2D isPlayerInRange;
+        [SerializeField] private TextAsset inkJSON;
+
+        [SerializeField] private LayerMask playerLayerMask;
+
+        private void Awake() {
+            visualCue.SetActive(false);
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
+        private void Update() {
+            isPlayerInRange = Physics2D.OverlapCircle(transform.position, 3, playerLayerMask);
+            if (isPlayerInRange) {
+                visualCue.SetActive(true);
+                if (Input.GetMouseButtonDown(0)) {
+                    Debug.Log($"This is the asset {inkJSON.text}");
+                }
+            }
+
+            else {
+                visualCue.SetActive(false);
+            }
         }
     }
 }
