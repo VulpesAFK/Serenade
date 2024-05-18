4b47bab046c76bfd936c633e335f9b3ea15c57bc
FoxParadeAFK <151231520+FoxParadeAFK@users.noreply.github.com>
2024-05-18 21:53:29 +0100

    Inkle Unity Integration + Scene Change + Script Instantiation

--- Serenade/Assets/Global C# Assets/Finite State Machine/Template/PlayerStateData.cs
+++ Serenade/Assets/Global C# Assets/Finite State Machine/Template/PlayerStateData.cs
@@ -2,7 +2,6 @@ public class PlayerStateData {
     public int amountOfJumpsLeft;
     public void ResetAmountOfJumps(int amount) => amountOfJumpsLeft = amount;
     public void DecreaseAmountOfJumpsLeft() => amountOfJumpsLeft--;
-
     public bool coyoteTime;
 
 }
