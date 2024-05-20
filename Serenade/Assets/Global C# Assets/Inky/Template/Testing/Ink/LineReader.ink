1f9d8f90bc55f9ec76f18783221d6bf0d0ee9ddd
FoxParadeAFK <151231520+FoxParadeAFK@users.noreply.github.com>
2024-05-20 10:14:06 +0100

    Self-Created Line Reader + Choices

--- Serenade/Assets/Global C# Assets/Inky/Template/Testing/Ink/LineReader.ink
+++ Serenade/Assets/Global C# Assets/Inky/Template/Testing/Ink/LineReader.ink
@@ -1,7 +1,22 @@
-This is a simple line one 
-This is then a simple line two
+How peculiar ... 
+A guest here at this time ...
 -> Third_line
 
 === Third_line === 
-This is a third line
+Lilithé is my name
+What business do you have with me?
+    + [Rosemondé, here to inquire about this fog]
+        -> Answer(1)
+    + [Just simply got lost]
+        -> Forth_line
+
+=== Forth_line ===
+Lost? Then little lamb, let me welcome you till we settle this issue
+    + [Thank you very much]
+        -> Answer(2)
+    + [How kind]
+        -> Answer(3)
+
+=== Answer(number) ===
+You chose option {number} 
 -> END
--- Serenade/Assets/Global C# Assets/Inky/Template/Testing/LineReader.cs
+++ Serenade/Assets/Global C# Assets/Inky/Template/Testing/LineReader.cs
@@ -1,21 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using Ink.Runtime;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-namespace FoxTail
+namespace FoxTail.Serenade.Experimental.Dialogue.Testing
 {
     public class LineReader : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
+        [SerializeField] private TextAsset inkJSON;
+        [SerializeField] private TextMeshProUGUI dialogueText;
+        [SerializeField] private GameObject[] dialogueChoseButtons;
+        private TextMeshProUGUI[] dialogueChose;
+        private Story currentStory;
+
+        private void Start() {
+            currentStory = new Story(inkJSON.text);
+
+            dialogueChose = new TextMeshProUGUI[dialogueChoseButtons.Length];
+            for (int index = 0; index < dialogueChoseButtons.Length; index++) {
+                dialogueChose[index] = dialogueChoseButtons[index].GetComponentInChildren<TextMeshProUGUI>();
+            }
+        }
+        private void Update() {
+            if (Input.GetKeyDown(KeyCode.D) && currentStory.canContinue) {
+                dialogueText.text = currentStory.Continue();;
+
+                if (currentStory.currentChoices.Count != 0) {
+                    EventSystem.current.SetSelectedGameObject(dialogueChoseButtons[0].gameObject);
+                    for (int index = 0; index < currentStory.currentChoices.Count; index++) {
+                        dialogueChoseButtons[index].gameObject.SetActive(true);
+                        dialogueChose[index].text = currentStory.currentChoices[index].text;
+                    }
+                }
+            }
+
+
+            if (currentStory.currentChoices.Count == 0) {
+                for (int index = 0; index < dialogueChoseButtons.Length; index++) {
+                    dialogueChoseButtons[index].gameObject.SetActive(false);
+                }  
+            }
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
+        public void HandleClicked(int choiceIndex) {
+            currentStory.ChooseChoiceIndex(choiceIndex);
+            dialogueText.text = currentStory.Continue();
+            if (currentStory.currentChoices.Count != 0) {
+                    EventSystem.current.SetSelectedGameObject(dialogueChoseButtons[0].gameObject);
+                    for (int index = 0; index < currentStory.currentChoices.Count; index++) {
+                        dialogueChoseButtons[index].gameObject.SetActive(true);
+                        dialogueChose[index].text = currentStory.currentChoices[index].text;
+                    }
+                }
         }
     }
 }
